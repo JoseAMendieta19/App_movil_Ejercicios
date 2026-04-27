bccd140fd5bcfe9ff8a203ad1774d751864d85f2
JoseAMendieta19 <josemendieta13a@gmail.com>
2026-04-26 20:27:51 -0500

    Implementación del ejercicio 2,4,8

--- app/src/main/java/com/example/app_movil_ejercicios/Ejercicio8.kt
+++ app/src/main/java/com/example/app_movil_ejercicios/Ejercicio8.kt
@@ -2,7 +2,11 @@ package com.example.app_movil_ejercicios
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +23,72 @@ class Ejercicio8 : AppCompatActivity() {
             insets
         }
 
+        val etCantidad = findViewById<EditText>(R.id.etCantidad)
+        val spOrigen = findViewById<Spinner>(R.id.spOrigen)
+        val spDestino = findViewById<Spinner>(R.id.spDestino)
+        val btnConvertir = findViewById<Button>(R.id.btnConvertir)
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
         val btnvolver8 = findViewById<Button>(R.id.btnvolver8)
+
+        // Lista de monedas
+        val monedas = arrayOf(
+            "Seleccione",
+            "USD (Dólar)",
+            "EUR (Euro)",
+            "PEN (Sol )",
+            "JPY (Yen Japones)",
+            "MXN (Peso mexicano)"
+        )
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, monedas)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spOrigen.adapter = adapter
+        spDestino.adapter = adapter
+
+        // Tasas respecto al USD
+        val tasasUSD = mapOf(
+            "USD (Dólar)" to 1.0,
+            "EUR (Euro)" to 0.92,
+            "PEN (Sol )" to 3.7,
+            "JPY (Yen Japones)" to 150.0,
+            "MXN (Peso mexicano)" to 17.0
+        )
+
+        btnConvertir.setOnClickListener {
+
+            val cantidad = etCantidad.text.toString().toDoubleOrNull()
+            val origen = spOrigen.selectedItem.toString()
+            val destino = spDestino.selectedItem.toString()
+
+            // Validaciones
+            if (cantidad == null) {
+                etCantidad.error = "Ingrese una cantidad"
+                return@setOnClickListener
+            }
+
+            if (cantidad <= 0) {
+                etCantidad.error = "Debe ser mayor a 0"
+                return@setOnClickListener
+            }
+
+            if (origen == "Seleccione" || destino == "Seleccione") {
+                tvResultado.text = "Seleccione ambas monedas"
+                return@setOnClickListener
+            }
+
+            // Convertir a USD
+            val enUSD = cantidad / tasasUSD[origen]!!
+
+            //  Convertir a moneda destino
+            val resultado = enUSD * tasasUSD[destino]!!
+
+            val resFormateado = String.format("%.2f", resultado)
+
+            tvResultado.text = "Resultado: $resFormateado $destino"
+        }
+
+        // Botón volver
 
         btnvolver8.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
