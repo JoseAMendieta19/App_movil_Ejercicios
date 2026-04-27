0769e74eee75a8494dae7a1a3f06c98b63f00bd8
paulina <lopezmarichica@gmail.com>
2026-04-26 19:29:04 -0500

    generador completado

--- app/src/main/java/com/example/app_movil_ejercicios/Ejercicio5.kt
+++ app/src/main/java/com/example/app_movil_ejercicios/Ejercicio5.kt
@@ -3,20 +3,49 @@ package com.example.app_movil_ejercicios
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Ejercicio5 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_ejercicio5)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val editTextLimite = findViewById<EditText>(R.id.editTextLimite)
+        val btnGenerar = findViewById<Button>(R.id.btnGenerar)
+        val textViewPrimos = findViewById<TextView>(R.id.textViewPrimos)
+
+        btnGenerar.setOnClickListener {
+
+            val limite = editTextLimite.text.toString().toIntOrNull()
+
+            if (limite != null && limite >= 2) {
+
+                var resultado = ""
+
+                for (num in 2..limite) {
+
+                    var esPrimo = true
+
+                    for (i in 2 until num) {
+                        if (num % i == 0) {
+                            esPrimo = false
+                            break
+                        }
+                    }
+
+                    if (esPrimo) {
+                        resultado += "$num "
+                    }
+                }
+
+                textViewPrimos.text = resultado
+
+            } else {
+                textViewPrimos.text = "Ingrese un número mayor o igual a 2"
+            }
         }
 
         val btnvolver5 = findViewById<Button>(R.id.btnvolver5)
